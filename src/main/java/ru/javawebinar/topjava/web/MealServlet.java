7775ff8bcc1cf8c1a4179eaf25c05229170cee93
Roman Zinnatov <rzinnatov@gmail.com>
2022-06-03 16:14:15 +0500

    add repository package and getAllMeals logic in MealServlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,28 +1,41 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
-
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.repository.MealRepoInMemoryImpl;
+import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.MealsUtil;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
-
     private static final Logger log = getLogger(MealServlet.class);
+
+    private static final int CALORIES_PER_DAY = 2000;
+
+    private final MealRepository meals;
+
+    public MealServlet() {
+        super();
+        meals = new MealRepoInMemoryImpl();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-        response.sendRedirect("meals.jsp");
+        List<MealTo> mealTos = MealsUtil.filteredByStreams(meals.getAll(), LocalTime.of(0, 0), LocalTime.of(23, 59), CALORIES_PER_DAY);
+
+        request.setAttribute("mealTos", mealTos);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
+
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,18 +1,42 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Roman
-  Date: 03.06.2022
-  Time: 10:10
-  To change this template use File | Settings | File Templates.
---%>
+<%@ page import="ru.javawebinar.topjava.util.TimeUtil" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html lang="ru">
 <head>
     <title>Meals</title>
+    <style>
+        .normal {
+            color: green;
+        }
+        .exceeded {
+            color: red;
+        }
+    </style>
 </head>
 <body>
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<table border="1">
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    </thead>
+    <tbody>
+    <c:forEach var="mealTo" items="${mealTos}">
+        <jsp:useBean id="mealTo" type="ru.javawebinar.topjava.model.MealTo"/>
+        <tr class="${mealTo.excess? 'exceeded':'normal'}">
+            <td>
+                <%=TimeUtil.formatDate(mealTo.getDateTime())%>
+            </td>
+            <td>${mealTo.description}</td>
+            <td>${mealTo.calories}</td>
+        </tr>
+    </c:forEach>
+    </tbody>
+</table>
 </body>
 </html>
