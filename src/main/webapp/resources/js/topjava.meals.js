7e02c28576938f29d6437963c74113a73fede701
Roman <98254722+RomanZin87@users.noreply.github.com>
2022-08-09 10:23:43 +0500

    HW09
    * add datetime-picker plugin

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -64,4 +64,29 @@ $(function () {
             }
         })
     );
+
+    $("#startDate").datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+    });
+
+    $("#endDate").datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+    });
+
+    $("#startTime").datetimepicker({
+        datepicker:false,
+        format:'H:i'
+    });
+
+    $("#endTime").datetimepicker({
+        datepicker:false,
+        format:'H:i'
+    });
+
+    $("#dateTime").datetimepicker({
+        datepicker: false,
+        format: 'Y-m-d H:i',
+    })
 });
