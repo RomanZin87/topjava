ecc6efcd9ef425ef29b0e3ac8f8ef85426ad7b77
Roman <98254722+RomanZin87@users.noreply.github.com>
2022-11-29 14:18:07 +0500

    add MealServlet and view

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,37 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MealServlet extends HttpServlet {
+
+    private static final Logger log = getLogger(MealServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Meal> meals = MealsUtil.meals;
+
+        Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
+                .collect(
+                        Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories)));
+
+        meals.forEach(meal -> MealsUtil.createTo(meal, caloriesSumByDate.get(meal.getDateTime().toLocalDate()) > MealsUtil.CALORIES_PER_DAY));
+        req.setAttribute("mealTos", meals);
+
+        log.debug("forward to meals");
+        req.getRequestDispatcher("meals.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,46 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Roman
+  Date: 29.11.2022
+  Time: 13:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Meals</title>
+    <style>
+        table {
+            width: 600px; /* Ширина таблицы */
+            border-collapse: collapse; /* Убираем двойные линии между ячейками */
+        }
+        td, th {
+            padding: 3px; /* Поля вокруг содержимого таблицы */
+            border: 1px solid black; /* Параметры рамки */
+        }
+        th {
+            background: #b0e0e6; /* Цвет фона */
+        }
+    </style>
+</head>
+<body>
+
+<h3><a href="index.html">Home</a></h3>
+<h2>Meals</h2>
+<table>
+    <tr>
+        <th>DateTime</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    <c:forEach items="${mealTos}" var="mealTo">
+        <tr>
+            <td>${mealTo.dateTime}</td>
+            <td>${mealTo.description}</td>
+            <td>${mealTo.calories}</td>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
