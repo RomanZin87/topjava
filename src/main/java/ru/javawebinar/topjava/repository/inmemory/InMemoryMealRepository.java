6e80052d4038da530fff3535e0b90afd6452bee7
Roman <98254722+RomanZin87@users.noreply.github.com>
2022-12-05 15:56:26 +0500

    hw002 first commit

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,46 +1,44 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
-    private final AtomicInteger counter = new AtomicInteger(0);
+@Repository
+public class InMemoryMealRepository extends InMemoryBaseRepository<Meal> implements MealRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
 
     {
         MealsUtil.meals.forEach(this::save);
     }
 
     @Override
-    public Meal save(Meal meal) {
-        if (meal.isNew()) {
-            meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
-            return meal;
-        }
-        // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+    public Meal save(Meal meal, int userId) {
+        log.info("save {}", meal);
+        return super.save(meal);
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int id, int userId) {
+        log.info("delete meal with id {}", id);
+        return super.delete(id);
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int id, int userId) {
+        log.info("get meal with id {}", id);
+        return super.get(id);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public Collection<Meal> getAll(int userId) {
+        log.info("getAll()");
+        return super.getAll();
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,43 +3,50 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractNamedEntity;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.util.UserUtils;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
-public class InMemoryUserRepository implements UserRepository {
+public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
+
+    {
+        UserUtils.users.forEach(this::save);
+    }
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return true;
+        return super.delete(id);
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return user;
+        return super.save(user);
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return super.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return super.getAll().stream().sorted(Comparator.comparing(AbstractNamedEntity::getName)).collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return null;
+        return super.getAll().stream().filter(u->u.getEmail().equals(email)).findFirst().orElse(null);
     }
 }
