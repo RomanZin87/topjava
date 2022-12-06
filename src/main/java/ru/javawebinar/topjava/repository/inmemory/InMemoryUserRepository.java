f2dbb7c21afd9eaf5bdcb46ad5ac7489734398b5
Roman <98254722+RomanZin87@users.noreply.github.com>
2022-12-06 09:39:23 +0500

    Revert "hw002 first commit"
    This reverts commit 6e80052d4038da530fff3535e0b90afd6452bee7.

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -3,50 +3,43 @@ package ru.javawebinar.topjava.repository.inmemory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
-import ru.javawebinar.topjava.model.AbstractNamedEntity;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.util.UserUtils;
 
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Repository
-public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
+public class InMemoryUserRepository implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
-
-    {
-        UserUtils.users.forEach(this::save);
-    }
 
     @Override
     public boolean delete(int id) {
         log.info("delete {}", id);
-        return super.delete(id);
+        return true;
     }
 
     @Override
     public User save(User user) {
         log.info("save {}", user);
-        return super.save(user);
+        return user;
     }
 
     @Override
     public User get(int id) {
         log.info("get {}", id);
-        return super.get(id);
+        return null;
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return super.getAll().stream().sorted(Comparator.comparing(AbstractNamedEntity::getName)).collect(Collectors.toList());
+        return Collections.emptyList();
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
-        return super.getAll().stream().filter(u->u.getEmail().equals(email)).findFirst().orElse(null);
+        return null;
     }
 }
