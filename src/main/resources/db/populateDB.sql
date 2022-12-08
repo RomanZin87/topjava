4ed8496a53bd39d3f2fe87b6b96c58085578b264
Roman <98254722+RomanZin87@users.noreply.github.com>
2022-12-08 15:24:52 +0500

    hw003 add spring-context init for tests, add SQL scripts, add JdbcMealRepository logic

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_roles;
-DELETE FROM users;
+DELETE
+FROM user_roles;
+DELETE
+FROM meals;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +14,9 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2022-12-08T10:00', 'Завтрак', 500),
+       (100000, '2022-12-08T12:00', 'Обед', 1000),
+       (100001, '2022-12-08T10:30', 'Завтрак', 700),
+       (100001, '2022-12-08T12:30', 'Обед', 900);
